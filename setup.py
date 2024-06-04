43c19f6e272cce5221d3efc53f86de1e8c9a35ac
Lolimpo <thelol1mpo@gmail.com>
2024-06-04 13:41:06 +0700

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="schemax",
-    version="0.2.0rc1",
+    version="0.2.0",
     description="district42 to JSON-Schema translator and vise versa",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
